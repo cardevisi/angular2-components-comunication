952db1296a6988576c20d924dc884d09cf67aa1f
Carlos Augusto Silveira de Oliveira <cardevisi@gmail.com>
2017-08-30 13:08:44 -0300

    [MASTER] adding a broadcast data.service

--- app/send-message/send-message.component.ts
+++ app/send-message/send-message.component.ts
@@ -1,18 +1,21 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { DataService } from "../data.service";
+
 
 @Component({
     selector: 'send-message',
     templateUrl: './send-message.component.html'
 })
 
-export class SendMessage {
+export class SendMessage implements OnInit {
     @Output() sendMessage = new EventEmitter();
     currentMessage:string = "Hello!";
+    message:string;
 
-    constructor() {
-        this.sendMessage.emit({msg:'SendByEmit'});
-        console.log('SendMessage');
+    constructor(private data: DataService) {
+        //this.sendMessage.emit({msg:'SendByEmit'});
+        //console.log('SendMessage');
     }
 
     clickToSend(event) {
@@ -21,8 +24,13 @@ export class SendMessage {
     };
 
     onSubmit(f: NgForm) {
-        console.log(f.value);  // { first: '', last: '' }
-        console.log(f.valid);  // false
+        console.log('f.value', f.value);  // { first: '', last: '' }
+        //console.log(f.valid);  // false
         this.sendMessage.emit({msg: f.value});
+        this.data.changeMessage(f.value.first);
+    }
+
+    ngOnInit() {
+      this.data.currentMessage.subscribe(message => this.message = message)
     }
 }
